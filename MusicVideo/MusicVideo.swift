2af18dd3d4b31e16db9c92559b5ea4521dbf4a01
Ravi Bijlani <nita@nitas-macbook-pro.local>
2016-06-11 01:08:37 -0400

    Completed Part-4

--- MusicVideo/MusicVideo.swift
+++ MusicVideo/MusicVideo.swift
@@ -13,8 +13,17 @@ class Videos {
     
     // Data Encapsulation
     private var _vName: String;
+    private var _vRights:String;
+    private var _vPrice:String;
+//    private var _vCurrency:String;
     private var _vImageUrl: String;
     private var _vVideoUrl: String;
+    private var _vArtist: String;
+    private var _vImid: String;
+    private var _vGenre: String;
+    private var _vLinkToiTunes: String;
+    private var _vReleaseDte: String;
+ 
     
     // make a getter
     
@@ -25,10 +34,47 @@ class Videos {
     var vImageUrl: String {
         return _vImageUrl;
     }
+ 
     var vVideoUrl: String {
         return _vVideoUrl;
     }
 
+    var vRights: String {
+        return _vRights;
+    }
+
+    var vPrice: String {
+        return _vPrice;
+    }
+
+//    var vCurrency: String {
+//        return _vCurrency;
+//    }
+
+    var vArtist: String {
+        return _vArtist;
+    }
+
+    var vImd: String {
+        return _vImid;
+    }
+
+    var vGenre: String {
+        return _vGenre;
+    }
+
+    var vLinkToiTunes: String {
+        return _vLinkToiTunes;
+    }
+
+    var vReleaseDte: String {
+        return _vReleaseDte;
+    }
+    
+    
+    
+    
+    
     init (data : JSONDictionary) {
         
         //Video Name
@@ -41,7 +87,29 @@ class Videos {
             self._vName = ""
         }
         
-        //Img Name
+        // Rights
+        if let rights = data ["rights"] as? JSONDictionary,
+            vRights = rights["label"] as? String{
+            self._vRights = vRights
+        } else {
+            // we initialized it to empty String
+            
+            self._vRights = ""
+        }
+
+        // Price & Currency
+        if let price = data ["im:price"] as? JSONDictionary,
+            vPrice = price["label"] as? String{
+            self._vPrice = vPrice
+        } else {
+            // we initialized it to empty String
+            
+            self._vPrice = ""
+        }
+       
+        
+        
+        //Img URL
         if let img = data["im:image"] as? JSONArray,
             image = img[2] as? JSONDictionary,
             immage = image["label"] as? String {
@@ -63,6 +131,61 @@ class Videos {
             
             self._vVideoUrl = ""
         }
-    
+        
+        
+        // Artist
+        if let artist = data ["im:artist"] as? JSONDictionary,
+            vArtist = artist["label"] as? String{
+            self._vArtist = vArtist
+        } else {
+            // we initialized it to empty String
+            
+            self._vArtist = ""
+        }
+      
+        // IMID
+        if let iMid = data ["id"] as? JSONDictionary,
+            vIdAttr = iMid["attributes"] as? JSONDictionary,
+            vImid = vIdAttr["im:id"] as? String {
+            self._vImid = vImid
+        } else {
+            // we initialized it to empty String
+            
+            self._vImid = ""
+        }
+        
+        //Genre
+        if let category = data ["category"] as? JSONDictionary,
+            vCatAttr = category["attributes"] as? JSONDictionary,
+            vGenre = vCatAttr["term"] as? String {
+            self._vGenre = vGenre
+        } else {
+            // we initialized it to empty String
+            
+            self._vGenre = ""
+        }
+ 
+        // Link to iTunes
+        if let iMid = data ["id"] as? JSONDictionary,
+            vLinkToiTunes = iMid["label"] as? String {
+            self._vLinkToiTunes = vLinkToiTunes
+        } else {
+            // we initialized it to empty String
+            
+            self._vLinkToiTunes = ""
+        }
+       
+        // Release Date
+        if let releaseDate = data ["im:releaseDate"] as? JSONDictionary,
+            vrDAttr = releaseDate["attributes"] as? JSONDictionary,
+            vReleaseDte = vrDAttr["label"] as? String {
+            self._vReleaseDte = vReleaseDte
+        } else {
+            // we initialized it to empty String
+            
+            self._vReleaseDte = ""
+        }
+     
+     
     }
 }
