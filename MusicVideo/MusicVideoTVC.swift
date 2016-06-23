1d5876f3319c0dad3c653dde414d5692d41e7a36
Ravi Bijlani <nita@nitas-macbook-pro.local>
2016-06-22 22:45:02 -0400

    Completed Part-12

---
+++ MusicVideo/MusicVideoTVC.swift
@@ -0,0 +1,137 @@
+//
+//  MusicVideoTVC.swift
+//  MusicVideo
+//
+//  Created by Ravi Bijlani on 6/22/16.
+//  Copyright © 2016 ROAR Smart. All rights reserved.
+//
+
+import UIKit
+
+class MusicVideoTVC: UITableViewController {
+
+    var videos = [Videos]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reachabilityStatusChanged", name: "ReachStatusChange", object: nil)
+        
+        reachabilityStatusChanged()
+        
+        // Call API
+        let api = APIManager()
+        api.loadData("https://itunes.apple.com/in/rss/topmusicvideos/limit=50/json", completion: didLoadData)
+    }
+    
+    
+    func reachabilityStatusChanged () {
+        
+        switch reachbilityStatus {
+        case NOACCESS : view.backgroundColor = UIColor.redColor()
+        //displayLabel.text = "No Internet"
+        case WIFI : view.backgroundColor = UIColor.greenColor()
+        //displayLabel.text = "Reachable with WIFI"
+        case NOACCESS : view.backgroundColor = UIColor.yellowColor()
+        //displayLabel.text = "Reachable with Cellular"
+        default:return
+        }
+    }
+    
+    // Is called just as object is about to be deallocated
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
+    }
+
+    func didLoadData(videos: [Videos]) {
+        
+        print(reachbilityStatus)
+        
+        self.videos = videos
+        
+        for item in videos {
+            print("name = \(item.vName)")
+        }
+        
+        for (index, item) in videos.enumerate() {
+            print("\(index) name = \(item.vName)")
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+
+    
+    
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return videos.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+
+        // Configure the cell...
+        let video = videos[indexPath.row]
+        
+        cell.textLabel?.text = ( "\(indexPath.row + 1)" )
+        cell.detailTextLabel?.text = video.vName
+        
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
