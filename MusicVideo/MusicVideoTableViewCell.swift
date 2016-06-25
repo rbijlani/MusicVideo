7a2ccbed57c4bd6d7f5055174ed0aef44212f264
Ravi Bijlani <nita@nitas-macbook-pro.local>
2016-06-25 09:41:12 -0400

    Completed Part-15

--- MusicVideo/MusicVideoTableViewCell.swift
+++ MusicVideo/MusicVideoTableViewCell.swift
@@ -26,9 +26,45 @@ class MusicVideoTableViewCell: UITableViewCell {
     func updateCell() {
         musicTitle.text = video?.vName
         rank.text = ("\(video!.vRank)")
-        musicImage.image = UIImage(named: "image_not_available")
+        //musicImage.image = UIImage(named: "image_not_available")
+        
+        if video?.vImageData != nil {
+            print("Get data from Array")
+            musicImage.image = UIImage(data: video!.vImageData!)
+        }
+        else {
+            GetVideoImage(video!, imageView: musicImage)
+        }
     }
     
     
+    
+    func GetVideoImage(video: Videos, imageView : UIImageView){
+        
+        // Background thread
+        //  DISPATCH_QUEUE_PRIORITY_HIGH Items dispatched to the queue will run at high priority, i.e. the queue will be scheduled for execution before any default priority or low priority queue.
+        //
+        //  DISPATCH_QUEUE_PRIORITY_DEFAULT Items dispatched to the queue will run at the default priority, i.e. the queue will be scheduled for execution after all high priority queues have been scheduled, but before any low priority queues have been scheduled.
+        //
+        //  DISPATCH_QUEUE_PRIORITY_LOW Items dispatched to the queue will run at low priority, i.e. the queue will be scheduled for execution after all default priority and high priority queues have been scheduled.
+        
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            
+            let data = NSData(contentsOfURL: NSURL(string: video.vImageUrl)!)
+            
+            var image : UIImage?
+            if data != nil {
+                video.vImageData = data
+                image = UIImage(data: data!)
+            }
+            
+            // move back to Main Queue
+            dispatch_async(dispatch_get_main_queue()) {
+                imageView.image = image
+            }
+        }
+    }
+
     
 }
